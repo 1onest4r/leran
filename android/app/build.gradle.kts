555fa48a1daffcc646bf2e4dbd29d15a3dc744a7
1onest4r <2024054@miu.edu.mn>
2026-04-03 12:34:31 +0800

    base setup for building the app

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,68 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+// 1. Define properties ONCE at the top
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "io.github.dev1onest4r.leran"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "io.github.dev1onest4r.leran"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            // Using getProperty is safer in Kotlin DSL
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storePassword = keystoreProperties.getProperty("storePassword")
+            
+            val stFile = keystoreProperties.getProperty("storeFile")
+            if (stFile != null) {
+                storeFile = rootProject.file(stFile)
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            // This links to the signingConfig defined above
+            signingConfig = signingConfigs.getByName("release")
+            
+            // Optimizations for Play Store
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/io/github/dev1onest4r/leran/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.dev1onest4r.leran
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
